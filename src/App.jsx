05c5638b3ce3f9d6ca012b55a957422cac862601
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2024-04-22 16:45:49 +0500

    First test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        TRAKTORBOR.UZ
+        <p>MTZ Belarus</p>
+        <p>YTO</p>
+        <p>Test rejim</p>
+      </h1>
+    </>
+  );
+}
+
+export default App;
