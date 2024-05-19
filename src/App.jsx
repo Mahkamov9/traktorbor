56c7bc7610cbaf75dbe18635073221d3a7849d06
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2024-05-19 13:55:17 +0500

    salom123

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-jhkml23
-  456456cjdjdfhdhjdhdssssssdxddddddfsfshfjfffdjddincjfjfjdxfdfjdhjcdjdhhddhdjdjdhdhfhshggvjhfdasasasSADlk456645sadendi boshlandi 
-gh
-ghff
 import './App.css';
 import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <>
+      <h1>Salom </h1>
+      <h2>sdasf</h2>
       <HomePage/>
     </>
   );
